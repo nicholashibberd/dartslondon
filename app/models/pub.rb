47a163b2e78ed0f4bb063869c7149e4d7df9460a
nicholashibberd <nicholashibberd@gmail.com>
2014-09-13 20:01:44 +1000

    Removing easy to get on board validation

--- app/models/pub.rb
+++ app/models/pub.rb
@@ -1,14 +1,6 @@
 class Pub < ActiveRecord::Base
   belongs_to :region
 
-  EASY_TO_GET_ON_BOARD_OPTIONS = [
-    ["Very Easy", 5],
-    ["Easy", 4],
-    ["Average", 3],
-    ["Not Easy", 2],
-    ["Ballache", 1],
-  ]
-
   MASS_ASSIGNABLE_FIELDS = [
     :name,
     :address,
@@ -22,7 +14,6 @@ class Pub < ActiveRecord::Base
     :quiz_machine,
   ]
 
-  validates :easy_to_get_on_board, numericality: true
   validates :number_of_dartboards, numericality: true
 
   def to_param
