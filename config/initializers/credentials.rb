d32a60f91e546ab875f9e33c265e1168e5884805
nicholashibberd <nicholashibberd@gmail.com>
2014-09-14 20:57:15 +1000

    Rescuing from Errno::ENOENT

--- config/initializers/credentials.rb
+++ config/initializers/credentials.rb
@@ -1,2 +1,6 @@
-credentials = YAML.load_file(Rails.root.join('config', 'credentials.yml')).with_indifferent_access
+begin
+  credentials = YAML.load_file(Rails.root.join('config', 'credentials.yml')).with_indifferent_access
+rescue Errno::ENOENT
+  credentials = {}
+end
 GOOGLE_MAPS_API_KEY = ENV['GOOGLE_MAPS_API_KEY'] || credentials[:google_maps_api_key]
