64181b523eaaf8a0a7a643f9aed32bb3d8884f2e
nicholashibberd <nicholashibberd@gmail.com>
2014-09-14 20:49:40 +1000

    Reworking config vars for heroku

--- config/initializers/credentials.rb
+++ config/initializers/credentials.rb
@@ -1,1 +1,2 @@
-CREDENTIALS = YAML.load_file(Rails.root.join('config', 'credentials.yml')).with_indifferent_access
+credentials = YAML.load_file(Rails.root.join('config', 'credentials.yml')).with_indifferent_access
+GOOGLE_MAPS_API_KEY = ENV['GOOGLE_MAPS_API_KEY'] || credentials[:google_maps_api_key]
