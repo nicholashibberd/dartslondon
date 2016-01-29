935bb04d9f174c4c1c387c56cb805381cbc35bad
“nicholashibberd” <nicholashibberd@gmail.com>
2016-01-29 08:42:38 +0000

    Add description to regions

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -77,6 +77,6 @@ class RegionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def region_params
-      params.require(:region).permit(:name)
+      params.require(:region).permit(:name, :description)
     end
 end
